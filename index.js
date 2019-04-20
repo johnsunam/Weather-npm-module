0eb25d34112f64579f91fa23114f9f9672bbb981
John suanam <sunamjohn@gmail.com>
2019-04-20 15:11:26 +0545

    changes

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const https = require('https');
+var https = require('https');
+var preference = require('./preference');
+var appid = '30173929657489149776555c35a4e6cc';
 
 const getForecast =  function () {
     return new Promise(function (resolve, reject){
-        https.get('https://api.openweathermap.org/data/2.5/weather?q=Munich&appid=30173929657489149776555c35a4e6cc', function(res){
-            res.on('data', function(data){
-                var data = JSON.parse(data);
-                console.log(data.weather)
-                resolve(data.weather);
+        https.get('https://api.openweathermap.org/data/2.5/weather?q='+preference.city+'&units='+preference.units+'&appid='+appid, function(res){
+            res.on('data', function(resData){
+                var resDataParse = JSON.parse(resData);
+                var data = {
+                    "weather": resDataParse.weather[0]['description'],
+                    "temperature": resDataParse.main.temp, 
+                    "pressure": resDataParse.main.pressure, 
+                    "humidity": resDataParse.main.humidity,
+                }
+                resolve(data);
             });
         }).end();
     });
 }
-getForecast();
 module.exports = getForecast;
